48a9aea746c7375613708ee85b2ea221dbec4342
Radosław Biesek <radoslaw.biesek@gmail.com>
2023-02-12 15:56:29 +0100

    add tailwind

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["ui"],
+  experimental: {
+    appDir: true,
+  },
 };
+
+module.exports = nextConfig;
---
+++ apps/web/src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Lista zakupów</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
