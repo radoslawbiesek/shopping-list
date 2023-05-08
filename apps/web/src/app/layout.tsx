8113320abd1106a81d692f8446970771709a1b9f
Radosław Biesek <radoslaw.biesek@gmail.com>
2023-05-08 21:10:21 +0200

    cleanup 2

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,14 +1,10 @@
-import { AppProviders } from '../../providers/AppProvider';
-
 import '../styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html data-theme="garden">
       <head />
-      <body className="h-screen w-screen p-0 m-0">
-        <AppProviders>{children}</AppProviders>
-      </body>
+      <body className="h-screen w-screen p-0 m-0">{children}</body>
     </html>
   );
 }
